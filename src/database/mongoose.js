91259f3449c275383164cebb8c4fbe3a43beff4f
EvanilsonP <pevanilson99@gmail.com>
2022-11-03 09:51:34 -0300

    Connecting to the database and creating model

---
+++ src/database/mongoose.js
@@ -0,0 +1,3 @@
+const mongoose = require('mongoose');
+// Establishing connection to the database
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager', {});
---
+++ src/models/user.model.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const User = mongoose.model('User', userSchema);
+// Creating a model and a schema;
+const userSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        validate(email) {
+            if(!validator.isEmail(email)) {
+                throw new Error('Email is invalid. Please try again.');
+            }
+        }
+    },
+
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7,
+        validate(password) {
+            if(password.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"');
+            }
+        }
+    },
+
+    age: {
+        required: true,
+        trim: true,
+        default: 0,
+        validate(age) {
+            if(age < 0) {
+                throw new Error('Age must be a positive number.');
+            }
+        }
+    }
+});
+
+module.exports = User;
